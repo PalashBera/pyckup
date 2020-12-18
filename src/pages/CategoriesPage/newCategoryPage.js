039e987077db5cf739a1c9581fb34dbec3a6c14f
Palash Bera <palashbera1234@gmail.com>
2020-12-18 21:02:31 +0530

    Adding new category paage with route

---
+++ src/pages/CategoriesPage/newCategoryPage.js
@@ -0,0 +1,13 @@
+import PageHeader from '../../components/PageHeader';
+
+function NewCategoryPage() {
+  return (
+    <>
+      <PageHeader title='New Category' />
+    </>
+  )
+}
+
+
+
+export default NewCategoryPage;
--- src/routes/index.js
+++ src/routes/index.js
@@ -5,12 +5,14 @@ import HomePage from '../pages/HomePage';
 import LoginPage from '../pages/LoginPage';
 import DashboardPage from '../pages/DashboardPage';
 import CategoriesPage from '../pages/CategoriesPage';
+import NewCategoryPage from '../pages/CategoriesPage/newCategoryPage';
 
 export default (
   <Switch>
     <Route exact path='/' component={HomePage} />
     <Route path='/login' component={LoginPage} />
     <AuthenticatedRoute path='/dashboard' component={DashboardPage} />
-    <AuthenticatedRoute path='/categories' component={CategoriesPage} />
+    <AuthenticatedRoute exact path='/categories' component={CategoriesPage} />
+    <AuthenticatedRoute exact path='/categories/new' component={NewCategoryPage} />
   </Switch>
 );
