6f442ccba11255ad4d6f6685d9665bfa80f7f4bf
Palash Bera <palashbera1234@gmail.com>
2020-12-18 21:02:00 +0530

    Fixing console error for action buttons

--- src/components/ActionButton/detailsAction.js
+++ src/components/ActionButton/detailsAction.js
@@ -4,8 +4,8 @@ import PropTypes from 'prop-types';
 function DetailsAction({ url }) {
   return (
     <Link className='actionButton' to={url}>
-      <svg xmlns='http://www.w3.org/2000/svg' xmlns-xlink='http://www.w3.org/1999/xlink' width='24px' height='24px' viewBox='0 0 24 24' version='1.1' class='svg-icon'>
-        <g stroke='none' stroke-width='1' fill='none' fill-rule='evenodd'>
+      <svg xmlns='http://www.w3.org/2000/svg' xmlns-xlink='http://www.w3.org/1999/xlink' width='24px' height='24px' viewBox='0 0 24 24' version='1.1'>
+        <g stroke='none' strokeWidth='1' fill='none' fillRule='evenodd'>
           <rect x='0' y='0' width='24' height='24'></rect>
           <path d='M7,3 L17,3 C19.209139,3 21,4.790861 21,7 C21,9.209139 19.209139,11 17,11 L7,11 C4.790861,11 3,9.209139 3,7 C3,4.790861 4.790861,3 7,3 Z M7,9 C8.1045695,9 9,8.1045695 9,7 C9,5.8954305 8.1045695,5 7,5 C5.8954305,5 5,5.8954305 5,7 C5,8.1045695 5.8954305,9 7,9 Z' fill='#000000'></path>
 
